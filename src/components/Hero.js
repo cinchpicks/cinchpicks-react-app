596907960924deb74d78c22998a6e37726c7a95e
Troy Johnson <troy.johnson@changepoint.com>
2020-03-31 20:01:11 -0700

    updated home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,22 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
+import logo from "../assets/football.png";
 
-const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+const Hero = () => ( <
+    div className = "text-center hero my-5" >
+    <
+    img className = "mb-3 app-logo"
+    src = { logo }
+    alt = "React logo"
+    width = "120" / >
+    <
+    h1 className = "mb-4" > Future home of Cinchpick < /h1>
 
-    <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
-    </p>
-  </div>
+    <
+    p className = "lead" >
+    Because we have jobs and a life but we still want fantasy football. < /
+    p > <
+    /div>
 );
 
 export default Hero;
