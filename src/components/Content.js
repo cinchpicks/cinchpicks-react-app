d22e4af84ba76008bb4dacbd08e06a8ae736b0df
Troy Johnson <troy.johnson@changepoint.com>
2020-04-01 20:47:25 -0700

    updated text

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,62 +7,16 @@ import contentData from "../utils/contentData";
 
 class Content extends Component {
     render() {
-        return ( <
-            div className = "next-steps my-5" >
-
+        return ( < div className = "next-steps my-5" >
             <
-            h2 className = "my-5 text-center" > What makes Cinchpicks different than other fantasy football leagues ? < /h2> 
-
-            <
+            h2 className = "my-5 text-center" > { " " }
+            What makes Cinchpicks different than other fantasy football leagues ? { " " } <
+            /h2> <
             p >
-            We realize not everyone has the time, or even the desire to spend hours researching players, teams, injury reports, weather and all the other things it takes to be successful in fantasy football leagues.Cinchpick lives up to it 's name </
-            p >
-
-
-
-            <
-            Row className = "d-flex justify-content-between" > {
-                contentData.map((col, i) => ( <
-                    Col key = { i }
-                    md = { 5 }
-                    className = "mb-4" >
-                    <
-                    h6 className = "mb-3" >
-                    <
-                    a href = { col.link } >
-                    <
-                    FontAwesomeIcon icon = "link"
-                    className = "mr-2" / > { col.title } <
-                    /a> < /
-                    h6 > <
-                    p > { col.description } < /p> < /
-                    Col >
-                ))
-            } <
-            /Row>
-
-            <
-            h2 className = "my-5 text-center" > What can I do next ? < /h2> <
-            Row className = "d-flex justify-content-between" > {
-                contentData.map((col, i) => ( <
-                    Col key = { i }
-                    md = { 5 }
-                    className = "mb-4" >
-                    <
-                    h6 className = "mb-3" >
-                    <
-                    a href = { col.link } >
-                    <
-                    FontAwesomeIcon icon = "link"
-                    className = "mr-2" / > { col.title } <
-                    /a> < /
-                    h6 > <
-                    p > { col.description } < /p> < /
-                    Col >
-                ))
-            } <
-            /Row> < /
-            div >
+            We realize not everyone has the time, or even the desire to spend hours researching players, teams, injury reports, weather and all the other things it takes to be successful in fantasy football leagues.Cinchpick lives up to it 's name by picking just one underdog
+            game a week.It doesn 't get any easier than that!{" "} < /
+            p > { " " } <
+            /div>
         );
     }
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Footer = () => (
-  <footer className="bg-light p-3 text-center">
-    <div className="logo" />
-    <p>
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
-    </p>
-  </footer>
+const Footer = () => ( <
+    footer className = "bg-light p-3 text-center" >
+    <
+    div className = "logo" / >
+    <
+    p >
+    Troy Johnson Designs <
+    /p> < /
+    footer >
 );
 
 export default Footer;
